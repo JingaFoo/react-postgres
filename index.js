3e4c54b998ffc8ee4d58e7817d4da0658eabc74b
JingaFoo <calvinmwariama@yahoo.com>
2018-06-12 22:04:57 -0400

    Disable Nodejs integration for security reasons

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const electron = require('electron');
 const { app, BrowserWindow } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+   mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: { 
+      nodeIntegration: false,
+      backgroundThrottling: false }
+  });
   mainWindow.loadURL('http://localhost:3000');
 });
